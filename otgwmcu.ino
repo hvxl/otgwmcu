37e6f7af822d04d0d49a4d360607bbe56bba9ae3
hvxl <hvxl@tclcode.com>
2022-05-23 21:08:22 +0200

    Reformat white space.

--- otgwmcu.ino
+++ otgwmcu.ino
@@ -22,112 +22,112 @@ WiFiManager wifiManager;
 // The Nodo shop OTGW hardware contains hardware that will reset
 // the NodeMCU unless the watchdog timer is cleared periodically.
 void clearwdt() {
-  Wire.beginTransmission(WDADDRESS);
-  Wire.write(WDPACKET);
-  Wire.endTransmission();
+    Wire.beginTransmission(WDADDRESS);
+    Wire.write(WDPACKET);
+    Wire.endTransmission();
 }
 
 void toggle() {
-  int state = digitalRead(LED1);
-  digitalWrite(LED1, !state);
+    int state = digitalRead(LED1);
+    digitalWrite(LED1, !state);
 }
 
 void blink(short delayms = 200) {
-  static Ticker blinker;
-  if (delayms) {
-    blinker.attach_ms(delayms, toggle);
-  } else {
-    blinker.detach();
-    digitalWrite(LED1, HIGH);
-  }
+    static Ticker blinker;
+    if (delayms) {
+        blinker.attach_ms(delayms, toggle);
+    } else {
+        blinker.detach();
+        digitalWrite(LED1, HIGH);
+    }
 }
 
 void fwupgradedone(OTGWError result, short errors = 0, short retries = 0) {
-  debuglog("Upgrade finished: Errorcode = %d - %d retries, %d errors\n", result, retries, errors);
-  switch (result) {
-   case OTGW_ERROR_NONE:
-    blink(0);
-    break;
-   case OTGW_ERROR_INPROG:
-    break;
-   case OTGW_ERROR_HEX_ACCESS:
-   case OTGW_ERROR_HEX_FORMAT:
-   case OTGW_ERROR_HEX_DATASIZE:
-   case OTGW_ERROR_HEX_CHECKSUM:
-   case OTGW_ERROR_MAGIC:
-    blink(500);
-    break;
-   default:
-    blink(100);
-    break;
-  }
+    debuglog("Upgrade finished: Errorcode = %d - %d retries, %d errors\n", result, retries, errors);
+    switch (result) {
+     case OTGW_ERROR_NONE:
+        blink(0);
+        break;
+     case OTGW_ERROR_INPROG:
+        break;
+     case OTGW_ERROR_HEX_ACCESS:
+     case OTGW_ERROR_HEX_FORMAT:
+     case OTGW_ERROR_HEX_DATASIZE:
+     case OTGW_ERROR_HEX_CHECKSUM:
+     case OTGW_ERROR_MAGIC:
+        blink(500);
+        break;
+     default:
+        blink(100);
+        break;
+    }
 }
 
 void fwupgradestart(const char *hexfile) {
-  OTGWError result;
-  
-  blink(0);
-  digitalWrite(LED1, LOW);
-  result = Pic.startUpgrade(hexfile);
-  if (result!= OTGW_ERROR_NONE) {
-    fwupgradedone(result);
-  } else {
-    Pic.registerFinishedCallback(fwupgradedone);
-  }
+    OTGWError result;
+
+    blink(0);
+    digitalWrite(LED1, LOW);
+    result = Pic.startUpgrade(hexfile);
+    if (result!= OTGW_ERROR_NONE) {
+        fwupgradedone(result);
+    } else {
+        Pic.registerFinishedCallback(fwupgradedone);
+    }
 }
 
 void setup() {
-  // Mount the file system
-  LittleFS.begin();
+    // Mount the file system
+    LittleFS.begin();
 
-  // Initialize the I/O ports
-  pinMode(BUTTON, INPUT_PULLUP);
-  pinMode(LED1, OUTPUT);
-  digitalWrite(LED1, HIGH);
-  pinMode(LED2, OUTPUT);
-  digitalWrite(LED2, HIGH);  
+    // Initialize the I/O ports
+    pinMode(BUTTON, INPUT_PULLUP);
+    pinMode(LED1, OUTPUT);
+    digitalWrite(LED1, HIGH);
+    pinMode(LED2, OUTPUT);
+    digitalWrite(LED2, HIGH);  
 
-  // Setup I2C for the watchdog
-  Wire.begin(I2CSDA, I2CSCL);
+    // Setup I2C for the watchdog
+    Wire.begin(I2CSDA, I2CSCL);
 
-  // Configure NTP before WiFi, so DHCP can override the NTP server(s)
-  configTime(TZ_Europe_Amsterdam, "pool.ntp.org");
+    // Configure NTP before WiFi, so DHCP can override the NTP server(s)
+    configTime(TZ_Europe_Amsterdam, "pool.ntp.org");
 
-  // Setup WiFi
-  wifiManager.setDebugOutput(false);
-  // wifiManager.resetSettings();
-  wifiManager.autoConnect("OTGW-MCU");
+    // Setup WiFi
+    wifiManager.setDebugOutput(false);
+    // wifiManager.resetSettings();
+    wifiManager.autoConnect("OTGW-MCU");
 
-  // Prepare the Serial to Network Proxy
-  proxysetup();
-  debugsetup();
-  websetup();
+    // Prepare the Serial to Network Proxy
+    proxysetup();
+    debugsetup();
+    websetup();
 }
 
 void loop() {
-  static unsigned int wdevent = 0, pressed = 0;
+    static unsigned int wdevent = 0, pressed = 0;
 
-  if (millis() - wdevent > WDTPERIOD) {
-    clearwdt();
-    wdevent = millis();
-  }
-
-  if (!Pic.busy()) {
-    if (digitalRead(BUTTON) == 0) {
-      if (pressed == 0) {
-        // In the very unlikely case that millis() happens to be 0, the 
-        // user will have to press the button for one millisecond longer.
-        pressed = millis();
-      } else if (millis() - pressed > 2000) {
-        fwupgradestart(FIRMWARE);
-        pressed = 0;
-      }
-    } else {
-      pressed = 0;
+    if (millis() - wdevent > WDTPERIOD) {
+        clearwdt();
+        wdevent = millis();
     }
-  }
 
-  proxyevent();
-  debugevent();
-  webevent();
+    if (!Pic.busy()) {
+        if (digitalRead(BUTTON) == 0) {
+            if (pressed == 0) {
+                // In the very unlikely case that millis() happens to be 0, the 
+                // user will have to press the button for one millisecond longer.
+                pressed = millis();
+            } else if (millis() - pressed > 2000) {
+                fwupgradestart(FIRMWARE);
+                pressed = 0;
+            }
+        } else {
+            pressed = 0;
+        }
+    }
+
+    proxyevent();
+    debugevent();
+    webevent();
 }
